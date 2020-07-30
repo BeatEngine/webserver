d8380b0bc1de7245ec5fdb7b9bf65cb5c3cb09be
BeatEngine <32369318+BeatEngine@users.noreply.github.com>
2020-07-30 11:57:18 +0200

    Version 1.0 (HTTP only and testversion)

---
+++ build-compile.bat
@@ -0,0 +1,2 @@
+g++ -I /include -L /lib -g main.cpp -w -lSDL2 -lboost_system -lboost_thread -lpthread -lssl -lcrypto -o webserverCpp.exe
+
---
+++ build-compile.sh
@@ -0,0 +1,3 @@
+cd projectFolder/
+g++ -I /include -L /lib -g main.cpp -w -lSDL2 -lboost_system -lboost_thread -lpthread -lssl -lcrypto -o webserverCpp
+
