e49bc2645e45b4be451994b53692616e6337998b
BeatEngine <32369318+BeatEngine@users.noreply.github.com>
2020-10-28 14:11:43 +0100

    Version 1.1 HTTPS / HTTP

---
+++ HttpRequest.h
@@ -0,0 +1,322 @@
+class StringMap
+{
+    std::vector<std::string> keys;
+    std::vector<std::string> values;
+
+    public:
+
+    size_t size()
+    {
+        return keys.size();
+    }
+
+    bool contains(std::string key)
+    {
+        for(int i = 0; i < keys.size(); i++)
+        {
+            if(keys[i] == key)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool put(std::string key, std::string value)
+    {
+        if(!contains(key))
+        {
+            keys.push_back(key);
+            values.push_back(value);
+            return true;
+        }
+        return false;
+    }
+
+    bool remove(std::string& key)
+    {
+        for(int i = 0; i < keys.size(); i++)
+        {
+            if(keys[i] == key)
+            {
+                keys.erase(keys.begin()+i);
+                values.erase(keys.begin()+i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    std::string get(std::string key)
+    {
+        for(int i = 0; i < keys.size(); i++)
+        {
+            if(keys[i] == key)
+            {
+                return values[i];
+            }
+        }
+        return "";
+    }
+
+    std::string get(long index)
+    {
+        return values.at(index);
+    }
+
+    std::string operator [](long index)
+    {
+        return values[index];
+    }
+
+    std::string keyAt(long index)
+    {
+        return keys.at(index);
+    }
+
+    bool set(std::string existingKey, std::string value)
+    {
+        for(int i = 0; i < keys.size(); i++)
+        {
+            if(keys[i] == existingKey)
+            {
+                values[i] = value;
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+
+class HttpRequest
+{
+
+    std::string toLowerCase(std::string input)
+    {
+        for(int i = 0; i < input.length(); i++)
+        {
+            if(input.at(i) < 91 && input.at(i) > 64)
+            {
+                input.at(i) += 32;
+            }
+        }
+        return input;
+    }
+
+    std::string toUpperCase(std::string input)
+    {
+        for(int i = 0; i < input.length(); i++)
+        {
+            if(input.at(i) < 123 && input.at(i) > 96)
+            {
+                input.at(i) -= 32;
+            }
+        }
+        return input;
+    }
+
+public:
+
+    std::string path;
+    std::string method;
+    StringMap attributes;
+    StringMap parameters;
+
+    HttpRequest()
+    {
+
+    }
+
+    std::string getQuery()
+    {
+        std::string result = "";
+        for(int i = 0; i < parameters.size(); i++)
+        {
+            if(i == 0)
+            {
+                result += '?';
+            }
+            result += parameters.keyAt(i);
+            result += parameters[i];
+            if(i + 1 < parameters.size())
+            {
+                result += "&";
+            }
+        }
+        return result;
+    }
+
+    std::string toString()
+    {
+        std::string generatedHeader = method + " " + path;
+        if(parameters.size() > 0 && method == "GET")
+        {
+            generatedHeader += "?";
+            for(int i = 0; i < parameters.size(); i++)
+            {
+                generatedHeader += parameters.keyAt(i) + "=" + parameters[i];
+                if(i < parameters.size()-1)
+                {
+                    generatedHeader += "&";
+                }
+            }
+        }
+        if(parameters.size() > 0 && method == "POST")
+        {
+            for(int i = 0; i < parameters.size(); i++)
+            {
+                generatedHeader += parameters.keyAt(i) + "=" + parameters[i];
+                if(i < parameters.size()-1)
+                {
+                    generatedHeader += "&";
+                }
+            }
+        }
+        generatedHeader += "\r\n";
+        if(attributes.size() > 0 && method == "GET")
+        {
+            for(int i = 0; i < attributes.size(); i++)
+            {
+                generatedHeader += attributes.keyAt(i) + ": " + attributes[i];
+                generatedHeader += "\r\n";
+            }
+        }
+        generatedHeader += "\r\n";
+        return generatedHeader;
+    }
+
+    
+
+    HttpRequest(std::string plainRequest)
+    {
+        int a = 0;
+        std::vector<std::string> lines;
+        for(int i = 0; i < plainRequest.length(); i++)
+        {
+            if(plainRequest.at(i) == '\r' && i + 1 < plainRequest.length())
+            {
+                if(plainRequest.at(i+1) == '\n')
+                {
+                    lines.push_back(plainRequest.substr(a, i-a));
+                    a = i+2;
+                }
+            }
+        }
+
+        for(int i = 0; i < lines.size(); i++)
+        {
+            if(i > 0)
+            {
+                std::string var;
+                std::string val;
+                a = 0;
+                for(int o = 0; o < lines.at(i).size(); o++)
+                {
+                    if(lines.at(i).at(o) == ':')
+                    {
+                        var = lines.at(i).substr(0, o);
+                        a = o + 1;
+                    }
+                }
+                if(a > 1)
+                {
+                    val = lines.at(i).substr(a, lines.at(i).size());
+
+                    if(val.at(0) == ' ')
+                    {
+                        val = val.substr(1, val.size());
+                    }
+                    attributes.put(toLowerCase(var), val);
+                }
+
+            }
+            else
+            {
+                a = 0;
+                int atr = 0;
+                int b = 0;
+                for(int o = 0; o < lines.at(0).size(); o++)
+                {
+                    if(lines.at(0).at(o) == ' ')
+                    {
+                        if(atr == 0)
+                        {
+                        method = lines.at(0).substr(a, o-a);
+                        method = toUpperCase(method);
+                        a = o + 1;
+                        }
+                        else if(atr == 1)
+                        {
+                            b = a+1;
+                            while(b < lines.at(0).length())
+                            {
+                                if(lines.at(0).at(b) == ' ')
+                                {
+                                    break;
+                                }
+                                b++;
+                            }
+
+                            path = lines.at(0).substr(a, b-a);
+                            break;
+                        }
+                        atr++;
+                    }
+                }
+
+                if(atr > 0)
+                {
+                    std::vector<std::string> params;
+                    a = 0;
+                    for(int c = 0; c < path.length(); c++)
+                    {
+                        if(path.at(c) == '?')
+                        {
+                            if(c + 1 < path.length())
+                            {
+                                std::string paras = path.substr(c+1, path.length());
+                                path = path.substr(0, c);
+                                a = 0;
+                                for(int u = 0; u < paras.length(); u++)
+                                {
+                                    if(paras.at(u) == '&')
+                                    {
+                                        params.push_back(paras.substr(a, u-a));
+                                        a = u + 1;
+                                    }
+                                }
+                                if(a < paras.length())
+                                {
+                                    params.push_back(paras.substr(a, paras.length()));
+                                }
+                            }
+                            break;
+                        }
+                    }
+
+                    std::string var;
+                    std::string val;
+                    for(int c = 0; c < params.size(); c++)
+                    {
+                        for(int h = 0; h < params.at(c).length(); h++)
+                        {
+                            if(params.at(c).at(h) == '=')
+                            {
+                                var = params.at(c).substr(0, h);
+                                val = params.at(c).substr(h+1, params.at(c).size());
+                                parameters.put(var, val);
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+    }
+
+
+};
+
--- main.cpp
+++ main.cpp
@@ -1,441 +1,254 @@
 //#include <stdio.h>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
-
 #include <boost/asio.hpp>
-#include <string>
-#include <ctime>
+#include <boost/asio/ssl.hpp>
+#define ssl_socket boost::asio::ssl::stream<boost::asio::ip::tcp::socket>
+#include "HttpRequest.h"
 
-#include <vector>
-
-#include <map>
-
-using std::string;
-
-void initOpenSSL()
+std::string generateResponsehead(long sizeBytes, HttpRequest& request, std::string customAttribute = "")
 {
-    SSL_load_error_strings();	
-    OpenSSL_add_ssl_algorithms();
-}
-
-void cleanupOpenSSL()
-{
-    EVP_cleanup();
-}
-
-SSL_CTX *create_context()
-{
-    const SSL_METHOD *method;
-    SSL_CTX *ctx;
-
-    method = SSLv23_server_method();
-
-    ctx = SSL_CTX_new(method);
-    if (!ctx) {
-	perror("Unable to create SSL context");
-	ERR_print_errors_fp(stderr);
-	exit(EXIT_FAILURE);
+    std::string type = "text/html; charset=utf-8";
+    if(request.attributes.contains("content-type"))
+    {
+        type = request.attributes.get("content-type");
     }
-
-    return ctx;
-}
-
-void configure_context(SSL_CTX *ctx, const char* certFile_PEM, const char* keyFile_PEM)
-{
-    SSL_CTX_set_ecdh_auto(ctx, 1);
-
-    /* Set the key and cert */
-    if (SSL_CTX_use_certificate_file(ctx, certFile_PEM, SSL_FILETYPE_PEM) <= 0) {
-        ERR_print_errors_fp(stderr);
-	exit(EXIT_FAILURE);
+    if(customAttribute.length() > 0)
+    {
+        customAttribute += "\r\n";
     }
-
-    if (SSL_CTX_use_PrivateKey_file(ctx, keyFile_PEM, SSL_FILETYPE_PEM) <= 0 ) {
-        ERR_print_errors_fp(stderr);
-	exit(EXIT_FAILURE);
-    }
+    std::string response = "HTTP/1.1 200 OK\r\n";
+    response += "content-type: " + type + "\r\n" + "content-length: " + std::to_string(sizeBytes) + "\r\n" + "Server: BeatEngine-Webserver\r\n" + customAttribute + "\r\n";
+    return response;
 }
 
-
-string read_(boost::asio::ip::tcp::socket & socket) 
+void handleHTTPSRequest(ssl_socket& server)
 {
-        boost::asio::streambuf buf;
-        boost::asio::read_until( socket, buf, "\n" );
-        string data = boost::asio::buffer_cast<const char*>(buf.data());
-        return data;
-}
-
-string read_SSL(boost::asio::ip::tcp::socket & socket, SSL* ssl) 
-{
-        char buff[1024] = {0};
-        int r = SSL_read(ssl, buff, 1024);
-        return string(buff);
-}
-
-void send_(boost::asio::ip::tcp::socket & socket, const string& message)
-{
-       boost::asio::write( socket, boost::asio::buffer((const void*)(message.c_str()), message.size()));
-}
-
-void send_SSL(boost::asio::ip::tcp::socket & socket, const string& message, SSL* ssl)
-{
-        int c = 0;
-        if(message.size() > 1024)
+    unsigned char buffer[2048];
+    int recv = 0;
+    int snd = 0;
+    long transferSize = 0;
+    long transfered = 0;
+    int w = 0;
+    long av;
+    HttpRequest request;
+    while (true)
+    {
+        av = server.next_layer().available();
+        if(av > 2048)
         {
-            while (c < message.size())
+            av = 2048;
+        }
+        if(av == 0)
+        {
+            int stp = 0;
+            while (av == 0)
             {
-                if(message.size() - c > 1024)
+                if(stp > 1000)
                 {
-                    c += SSL_write(ssl, message.c_str(), 1024);
-                }
-                else
-                {
-                    c += SSL_write(ssl, message.c_str(), message.size() - c);
-                }
-            }
-        }
-        else
-        {
-            c += SSL_write(ssl, message.c_str(), message.size());
-        }
-}
-
-void send_file(boost::asio::ip::tcp::socket & socket, const string& path)
-{
-    //printf("File: %s\n", path.c_str());
-    FILE* f = fopen(path.c_str(), "rb");
-    if(f == NULL)
-    {
-        send_(socket, string("HTTP/1.1 404 Not Found\r\n\r\n"));
-        return;
-    }
-    long length = 0;
-    fseek(f, 0, SEEK_END);
-    length = ftell(f);
-    fseek(f, 0, SEEK_SET);
-    long snd = 0;
-    unsigned char* buffer = (unsigned char*)calloc(sizeof(unsigned char), 1024);
-    size_t szChar = sizeof(unsigned char);
-    size_t read;
-    
-    char fsz[10];
-    sprintf (fsz, "%d", length);
-    send_(socket, string("HTTP/1.1 200 OK\r\ncontent-length: " + string(fsz) + "\r\n\r\n"));
-
-    while (length > 0)
-    {
-        if(length < 1024)
-        {
-            read = fread(buffer, szChar, length, f);
-        }
-        else
-        {
-            read = fread(buffer, szChar, 1024, f);
-        }
-        boost::asio::write( socket, boost::asio::buffer((const void*)(buffer), read));
-        length -= read;
-    }
-    free(buffer);
-    fclose(f);
-}
-
-void send_file_SSL(boost::asio::ip::tcp::socket & socket, const string& path, SSL* ssl)
-{
-    //printf("File: %s\n", path.c_str());
-    FILE* f = fopen(path.c_str(), "rb");
-    if(f == NULL)
-    {
-        send_SSL(socket, string("HTTP/1.1 404 Not Found\r\n\r\n"), ssl);
-        return;
-    }
-    long length = 0;
-    fseek(f, 0, SEEK_END);
-    length = ftell(f);
-    fseek(f, 0, SEEK_SET);
-    long snd = 0;
-    unsigned char* buffer = (unsigned char*)calloc(sizeof(unsigned char), 1024);
-    size_t szChar = sizeof(unsigned char);
-    size_t read;
-    
-    char fsz[10];
-    sprintf (fsz, "%d", length);
-    send_SSL(socket, string("HTTP/1.1 200 OK\r\ncontent-length: " + string(fsz) + "\r\n\r\n"), ssl);
-
-    while (length > 0)
-    {
-        if(length < 1024)
-        {
-            read = fread(buffer, szChar, length, f);
-        }
-        else
-        {
-            read = fread(buffer, szChar, 1024, f);
-        }
-        SSL_write(ssl, buffer, read);
-        length -= read;
-    }
-    free(buffer);
-    fclose(f);
-}
-
-class HttpRequest
-{
-
-    string toLowerCase(string input)
-    {
-        for(int i = 0; i < input.length(); i++)
-        {
-            if(input.at(i) < 91 && input.at(i) > 64)
-            {
-                input.at(i) += 32;
-            }
-        }
-    }
-
-    string toUpperCase(string input)
-    {
-        for(int i = 0; i < input.length(); i++)
-        {
-            if(input.at(i) < 123 && input.at(i) > 96)
-            {
-                input.at(i) -= 32;
-            }
-        }
-    }
-
-public:
-
-    string path;
-    string method;
-    std::map<string, string> attributes;
-    std::map<string, string> parameters;
-
-    HttpRequest()
-    {
-
-    }
-
-    
-
-    HttpRequest(string plainRequest)
-    {
-        int a = 0;
-        std::vector<string> lines;
-        for(int i = 0; i < plainRequest.length(); i++)
-        {
-            if(plainRequest.at(i) == '\r' && i + 1 < plainRequest.length())
-            {
-                if(plainRequest.at(i+1) == '\n')
-                {
-                    lines.push_back(plainRequest.substr(a, i-a));
-                    a = i+2;
-                }
-            }
-        }
-
-        for(int i = 0; i < lines.size(); i++)
-        {
-            if(i > 0)
-            {
-                string var;
-                string val;
-                a = 0;
-                for(int o = 0; o < lines.at(i).size(); o++)
-                {
-                    if(lines.at(i).at(o) == ':')
+                    if(transfered == 0)
                     {
-                        var = lines.at(i).substr(0, o);
-                        a = o + 1;
+                        break;
                     }
+                    return;
                 }
-                if(a > 1)
-                {
-                    val = lines.at(i).substr(a, lines.at(i).size());
-
-                    if(val.at(0) == ' ')
-                    {
-                        val = val.substr(1, val.size());
-                    }
-                    attributes.insert(std::pair<string,string>(toLowerCase(var), val));
-                }
-
+                usleep(100);
+                av = server.next_layer().available();
+                stp ++;
             }
-            else
+            if(av > 2048)
             {
-                a = 0;
-                int atr = 0;
-                int b = 0;
-                for(int o = 0; o < lines.at(0).size(); o++)
+                av = 2048;
+            }
+        }
+        try
+        {
+            recv = server.read_some(boost::asio::buffer(buffer, av));
+        }
+        catch(std::exception e)
+        {
+            return;
+        }
+        if(transfered == 0)
+        {
+            /*std::string buf((const char*)buffer);
+            int h = buf.find("Host:") + 4;
+            while(buffer[h] != '\r' && buffer[h] != '\n' && h > 4)
+            {
+                if(buffer[h] == ':')
                 {
-                    if(lines.at(0).at(o) == ' ')
+                    std::string np = destination.next_layer().remote_endpoint().address().to_string()+":"+std::to_string(targetport);
+                    int p = h;
+                    for(int i = 0; i < np.length(); i++)
                     {
-                        if(atr == 0)
+                        buffer[h+1+i] = np[i];
+                        p = h+1+i;
+                    }
+                    p++;
+                    while (buffer[p] != '\r' && buffer[p] != '\n')
+                    {
+                        buffer[p] = ' ';
+                        p++;
+                    }
+                    break;
+                }
+                h++;
+            }*/
+            request = HttpRequest(std::string((char*)buffer));
+            if(recv > 0 && request.method == "GET")
+            {
+                printf("%s %s %s   %s\n",request.method.c_str(), request.path.c_str(), request.getQuery().c_str(), request.attributes.get("content-type").c_str());
+            }
+        }
+        if(recv <= 0)
+        {
+            break;
+        }
+        transferSize += recv;
+    }
+    if(transferSize > 0)
+    {
+        transfered = 0;
+        snd = 0;
+        if(request.method == "GET")
+        {
+            std::string path = request.path;
+            if(path == "/")
+            {
+                path = "index.html";
+            }
+            if(path[0] == '/')
+            {
+                path = path.substr(1);
+            }
+            FILE* f = fopen(path.c_str(), "rb");
+            if(f == 0)
+            {
+                snd = 0;
+                std::string notFound = "HTTP/1.1 404 Not Found\r\n\r\n<html><head><title>Not Found 404</title></head><body><h1>Not Found!</h1></body></html>";
+                printf("Not found!\n");
+                try
+                {
+                    while (snd < notFound.length())
+                    {
+                        snd = server.write_some(boost::asio::buffer(notFound.c_str()+snd, notFound.size()-snd));
+                        if(snd == -1)
                         {
-                        method = lines.at(0).substr(a, o-a);
-                        method = toUpperCase(method);
-                        a = o + 1;
-                        }
-                        else if(atr == 1)
-                        {
-                            b = a+1;
-                            while(b < lines.at(0).length())
-                            {
-                                if(lines.at(0).at(b) == ' ')
-                                {
-                                    break;
-                                }
-                                b++;
-                            }
-
-                            path = lines.at(0).substr(a, b-a);
                             break;
                         }
-                        atr++;
+                        transfered += snd;
                     }
                 }
-
-                if(atr > 0)
+                catch(std::exception e)
                 {
-                    std::vector<string> params;
-                    a = 0;
-                    for(int c = 0; c < path.length(); c++)
+                    return;
+                }
+                return;
+            }
+            fseek(f, 0L, SEEK_END);
+            long size = ftell(f);
+            printf("Size:%d\n", size);
+            fseek(f, 0L, SEEK_SET);
+            std::string responseHead = generateResponsehead(size, request);
+            snd = 0;
+            try
+            {
+                while (snd < responseHead.length())
+                {
+                    snd += server.write_some(boost::asio::buffer(responseHead.c_str()+snd, responseHead.size()-snd));
+                    if(snd == -1)
                     {
-                        if(path.at(c) == '?')
+                        break;
+                    }
+                    transfered += snd;
+                }
+            }
+            catch(std::exception e)
+            {
+                fclose(f);
+                return;
+            }
+            long r = 0;
+            long tr = 0;
+            try
+            {
+                while (tr < size)
+                {
+                    r = 0;
+                    if(size - r > 2048)
+                    {
+                        r = fread(buffer, 1, 2048, f);
+                    }
+                    else
+                    {
+                        r = fread(buffer, 1, size-r, f);
+                    }
+                    tr += r;
+                    snd = 0;
+                    while (snd < r)
+                    {
+                        snd += server.write_some(boost::asio::buffer(buffer+snd, r-snd));
+                        if(snd == -1)
                         {
-                            if(c + 1 < path.length())
-                            {
-                                string paras = path.substr(c+1, path.length());
-                                path = path.substr(0, c);
-                                a = 0;
-                                for(int u = 0; u < paras.length(); u++)
-                                {
-                                    if(paras.at(u) == '&')
-                                    {
-                                        params.push_back(paras.substr(a, u-a));
-                                        a = u + 1;
-                                    }
-                                }
-                                if(a < paras.length())
-                                {
-                                    params.push_back(paras.substr(a, paras.length()));
-                                }
-                            }
                             break;
                         }
-                    }
-
-                    string var;
-                    string val;
-                    for(int c = 0; c < params.size(); c++)
-                    {
-                        for(int h = 0; h < params.at(c).length(); h++)
-                        {
-                            if(params.at(c).at(h) == '=')
-                            {
-                                var = params.at(c).substr(0, h);
-                                val = params.at(c).substr(h+1, params.at(c).size());
-                                parameters.insert(std::pair<string,string>(var, val));
-                                break;
-                            }
-                        }
+                        transfered += snd;
                     }
                 }
+            }
+            catch(std::exception e)
+            {
+                fclose(f);
+                return;
+            }
+            fclose(f);
+        }
+        else
+        {
+            /* code */
+        }
+    }
 
+}
+
+void server(int port, bool https = true)
+{
+    if(https)
+    {
+        boost::asio::ip::tcp::endpoint serverV4(boost::asio::ip::tcp::v4(), port);
+        boost::asio::ip::tcp::endpoint serverV6(boost::asio::ip::tcp::v6(), port);
+        boost::asio::io_service io_service;
+        boost::asio::ip::tcp::acceptor acceptorV4(io_service, serverV4);
+        //boost::asio::ip::tcp::acceptor acceptorV6(io_service, serverV6);
+        boost::asio::ssl::context ssl_context(boost::asio::ssl::context::tlsv13_server);
+        ssl_context.use_certificate_file("certs/newcert.pem", boost::asio::ssl::context_base::pem);
+        ssl_context.use_private_key_file("certs/privkey.pem", boost::asio::ssl::context_base::pem);
+        ssl_context.use_tmp_dh_file("certs/dh2048.pem");
+
+        while(true)
+        {
+            try
+            {
+                ssl_socket socket(io_service, ssl_context);
+                acceptorV4.accept(socket.next_layer());
+                socket.handshake(boost::asio::ssl::stream_base::handshake_type::server);
+                handleHTTPSRequest(socket);
+                socket.next_layer().close();
+            }
+            catch(const std::exception& e)
+            {
+                printf("Connection Error!\n");
             }
         }
 
     }
-
-
-};
-
+}
 
 int main(int args, char** arg)
 {
-    int port = 80;
-    boost::asio::io_service io_service;
-//listen for new connection
-    boost::asio::ip::tcp::acceptor acceptor(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port ));
-//socket creation 
+    int port = 8080;
+
+    server(port);
 
 
-    SSL_CTX *ctx;
-    initOpenSSL();
-    ctx = create_context();
-    //configure_context(ctx, "./ca-certificates/CertFile.crt", "./ca-certificates/KeyFile.pem");
-
-    boost::asio::ip::tcp::socket socket(io_service);
-//waiting for connection
-    string dir = ".";
-    if(args > 0)
-    {
-        string dir = string(arg[0]);
-    }
-
-    
-    printf("Server running! Port: %d\n", port);
-    
-    while(true)
-    {   
-        acceptor.accept(socket);
-
-        //SSL* ssl;
-        if (false /*SSL_accept(ssl) <= 0*/) {
-            ERR_print_errors_fp(stderr);
-        }
-        else {
-        string message = read_(socket);
-        string target = dir;
-        HttpRequest request(message);
-
-
-        printf("Method: %s\n", request.method.c_str());
-        printf("Path: %s\n", request.path.c_str());
-        printf("Attributes: %d\n", request.attributes.size());
-        printf("Parameters: %d\n", request.parameters.size());
-        
-        if(request.path == "/")
-        {
-            request.path = "/index.html";
-        }
-        target.append(request.path);
-
-        try
-        {
-            if(request.method == "GET")
-            {
-                send_file(socket, target);
-            }
-            else if(request.method == "POST")
-            {
-
-            }
-            else
-            {
-                send_file(socket, dir + "/index.html");
-            }
-            
-            socket.shutdown(socket.shutdown_both);
-            socket.close();
-        }
-        catch( const boost::system::system_error& ex )
-        {
-            printf("Connection lost (%s)\n", target.c_str());
-            ex.what();
-            socket.close();
-        }
-
-
-        }
-        //SSL_shutdown(ssl);
-        //SSL_free(ssl);
-        
-    }
-    SSL_CTX_free(ctx);
-    cleanupOpenSSL();
-    printf("Exit!\n");
     return 0;
 }
-
-
 
