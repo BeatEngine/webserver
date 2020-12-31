8e838d92ded6729057c5127e119e6d87d262a2bd
BeatEngine <32369318+BeatEngine@users.noreply.github.com>
2020-12-31 14:11:36 +0100

    Build RaspberryPi compatible

--- build-compile_raspberrypi.sh
+++ build-compile_raspberrypi.sh
@@ -4,4 +4,4 @@ if dpkg -s libboost1.62-all-dev | grep "Status:" = "Status: install ok installed
   else
    sudo apt install libboost1.62-all-dev
 fi
-g++ -I include -L lib -g main.cpp -w -static-libstdc++ -static-libgcc lib/libboost_system.a lib/libboost_thread.a lib/libboost_filesystem.a -lpthread -lssl -lcrypto -o webserverCpp
+g++ -I include -g main.cpp -w -static-libstdc++ -static-libgcc lib/libboost_system.a lib/libboost_thread.a -lboost_filesystem -lpthread -lssl -lcrypto -o webserverCpp
