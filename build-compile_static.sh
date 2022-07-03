0c9a19432bdc4c550b1091f674bc0fe295906e16
BeatEngine <32369318+BeatEngine@users.noreply.github.com>
2022-07-03 12:23:27 +0200

    Delete build-compile_static.sh

--- build-compile_static.sh
+++
@@ -1,2 +0,0 @@
-g++ -I include -L lib -g main.cpp -w -static-libstdc++ -static-libgcc lib/libboost_system.a lib/libboost_thread.a lib/libboost_filesystem.a -lpthread -lssl -lcrypto -o webserverCpp
-
