0e54f799da9874f533e272a3c74fbfa928f9eded
BeatEngine <32369318+BeatEngine@users.noreply.github.com>
2022-07-03 12:23:24 +0200

    Delete build-compile_raspberrypi.sh

--- build-compile_raspberrypi.sh
+++
@@ -1,7 +0,0 @@
-if dpkg -s libboost1.62-all-dev | grep "Status:" = "Status: install ok installed"
-  then
-   echo "Checked dependencies!\n"
-  else
-   sudo apt install libboost1.62-all-dev
-fi
-g++ -I include -g main.cpp -w -static-libstdc++ -static-libgcc lib/libboost_system.a lib/libboost_thread.a -lboost_filesystem -lpthread -lssl -lcrypto -o webserverCpp
